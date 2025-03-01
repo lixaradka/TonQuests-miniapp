affc615851f3ee3914049d84d54028671513852b
nikon <85578630+lixaradka@users.noreply.github.com>
2025-03-01 22:30:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function showTab(tabId) {
+    // Скрываем все вкладки
+    const tabs = document.querySelectorAll('.tab-content');
+    tabs.forEach(tab => {
+        tab.classList.remove('active');
+    });
+    // Показываем выбранную вкладку
+    document.getElementById(tabId).classList.add('active');
+}
+
+// Обновляем страницу каждые 60 секунд
 setInterval(function() {
     location.reload();
-}, 60000); // Обновление каждые 60 секунд
+}, 60000);
